8c481dfd3a29a86d5f7f6f04549a82f0976dc909
psw35 <psw35@nau.edu>
2025-04-26 02:13:10 -0700

    Added data folder files/code, added nav graph, and a few other changes

---
+++ app/src/main/java/com/nau/releasefinder/data/database/ReleaseDao.kt
@@ -0,0 +1,24 @@
+package com.nau.releasefinder.data.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.nau.releasefinder.data.database.model.Release
+import kotlinx.coroutines.flow.Flow
+
+// declare a dao
+@Dao
+interface ReleaseDao {
+    // insert a release
+    @Insert
+    suspend fun insertRelease(release: Release)
+
+    // query the release with a select statement
+    @Query("SELECT * FROM releaseTable")
+    suspend fun getRelease(): Flow<Release>
+
+    // delete a release
+    @Delete
+    suspend fun deleteRelease(releaseToDelete: Release)
+}
---
+++ app/src/main/java/com/nau/releasefinder/network/RetrofitClient.kt
@@ -0,0 +1,4 @@
+package com.nau.releasefinder.network
+
+object RetrofitClient {
+}
