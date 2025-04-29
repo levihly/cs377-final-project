9af2ec5ef62ee0f4ef1286bc7973371caaaa49cb
psw35 <psw35@nau.edu>
2025-04-28 23:31:21 -0700

    Added HomeFragment and some navigation between fragments

--- app/src/main/java/com/nau/releasefinder/ui/MainActivity.kt
+++ app/src/main/java/com/nau/releasefinder/ui/MainActivity.kt
@@ -5,17 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import com.nau.releasefinder.R
 
 class MainActivity : AppCompatActivity() {
+
+    // make variable for the navigation controller
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // set up nav controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
     }
 }
--- app/src/main/java/com/nau/releasefinder/ui/viewmodel/ReleaseViewModel.kt
+++ app/src/main/java/com/nau/releasefinder/ui/viewmodel/ReleaseViewModel.kt
@@ -1,23 +1,38 @@
 package com.nau.releasefinder.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.nau.releasefinder.data.database.model.Release
 import com.nau.releasefinder.data.repository.ReleaseRepository
+import com.nau.releasefinder.ui.HomeFragmentDirections
 import kotlinx.coroutines.launch
 
 class ReleaseViewModel(private val repository: ReleaseRepository) : ViewModel() {
     private val _title = MutableLiveData<String>()
+    private lateinit var navController: NavController
     val title: LiveData<String> get() = _title
 
     fun fetchRelease(id: String) {
         viewModelScope.launch {
             try {
+                // get the API response
                 val response = repository.fetchRelease(id)
-                _title.value = response.title
+                // get the release returned
+                val release: Release = response.release
+                _title.value = release.title
+
+                // go to details w/ the release
+                val action = HomeFragmentDirections.actionHomeFragmentToDetailsFragment(release)
+                navController.navigate(action)
+
             } catch (exception: Exception) {
                 // handle error
+                Log.d("fetchRelease", exception.toString())
             }
         }
     }
